caa07c3f6885ac616ae837ce9cb03e2246acc6e6
jiren <yujienjoyfever@yahoo.co.jp>
2022-05-20 16:53:07 +0900

    REQUIRES_NEW

--- src/main/java/com/volkruss/transactiontest/application/service/stock/impl/StockServiceImpl.java
+++ src/main/java/com/volkruss/transactiontest/application/service/stock/impl/StockServiceImpl.java
@@ -1,10 +1,41 @@
 package com.volkruss.transactiontest.application.service.stock.impl;
 
 import com.volkruss.transactiontest.application.service.stock.StockService;
+import com.volkruss.transactiontest.domain.model.stock.Stock;
+import com.volkruss.transactiontest.domain.repository.stock.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+@Service
 public class StockServiceImpl implements StockService {
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @Autowired
+    private PlatformTransactionManager txManager;
+
     @Override
     public void update() {
-
+        //　あくまでトランザクション確認用のメソッドです。
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW); //新規トランザクションを開始
+        def.setIsolationLevel(TransactionDefinition.ISOLATION_DEFAULT);
+        def.setTimeout(10);
+        def.setReadOnly(false);
+        TransactionStatus status = txManager.getTransaction(def);
+        try{
+            Stock stock = this.stockRepository.findStockByItemId(2);
+            stock.subCount(10);
+            stockRepository.save(stock);
+        } catch (RuntimeException e){
+            txManager.rollback(status);
+            throw e;
+        }
+        txManager.commit(status);
     }
 }
