31c326faefce48332734c751561638d2c02d11fd
jiren <yujienjoyfever@yahoo.co.jp>
2022-05-20 12:25:26 +0900

    宣言的トランザクション（アノテーション）の実装

--- src/main/java/com/volkruss/transactiontest/application/service/accept/impl/AcceptServiceImpl.java
+++ src/main/java/com/volkruss/transactiontest/application/service/accept/impl/AcceptServiceImpl.java
@@ -9,11 +9,7 @@ import com.volkruss.transactiontest.domain.repository.item.ItemRepository;
 import com.volkruss.transactiontest.domain.repository.stock.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
-
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AcceptServiceImpl implements AcceptService {
@@ -27,44 +23,26 @@ public class AcceptServiceImpl implements AcceptService {
     @Autowired
     private StockRepository stockRepository;
 
-    @Autowired
-    private PlatformTransactionManager txManager;
-
     @Override
+    @Transactional
     public void createAccept(AcceptController.RegistAcceptModel model) throws Exception {
+        // ドメインモデルの作成
+        Accept accept = new Accept(model.itemId, model.customerName, model.count);
+        // 受注の登録をする
+        // わかりやすくロールバックを確認するためここで永続化します
+        this.acceptRepository.register(accept);
 
-        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
-        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-        def.setIsolationLevel(TransactionDefinition.ISOLATION_DEFAULT);
-        def.setTimeout(10);
-        def.setReadOnly(false);
-        TransactionStatus status = txManager.getTransaction(def);
-        try{
-            // ドメインモデルの作成
-            Accept accept = new Accept(model.itemId,model.customerName,model.count);
-            // 受注の登録をする
-            // わかりやすくロールバックを確認するためここで永続化します
-            this.acceptRepository.register(accept);
+        // 在庫の取得
+        Stock stock = this.stockRepository.findStockByItemId(accept.getItemId());
 
-            // 在庫の取得
-            Stock stock = this.stockRepository.findStockByItemId(accept.getItemId());
-
-            // 在庫を減らす(本当の業務なら実在庫を減らすことはせず予定在庫を減らすなど)
-            stock.subCount(accept.getCount());
-            // 在庫の整合性を確認する
-            if(stock.isMinus()){
-                // 適当な例外投げてます
-                throw new Exception();
-            }
-            // 在庫の更新をする
-            this.stockRepository.save(stock);
-
-
-        } catch (RuntimeException e){
-            txManager.rollback(status);
-            throw e;
+        // 在庫を減らす(本当の業務なら実在庫を減らすことはせず予定在庫を減らすなど)
+        stock.subCount(accept.getCount());
+        // 在庫の整合性を確認する
+        if (stock.isMinus()) {
+            // 適当な例外投げてます
+            throw new RuntimeException();
         }
-        txManager.commit(status);
-
+        // 在庫の更新をする
+        this.stockRepository.save(stock);
     }
 }
