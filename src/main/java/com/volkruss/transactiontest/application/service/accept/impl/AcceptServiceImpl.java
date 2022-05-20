2d0155af50db73c8085b075793c113e975228ed0
jiren <yujienjoyfever@yahoo.co.jp>
2022-05-20 16:53:07 +0900

    REQUIRES_NEW

--- src/main/java/com/volkruss/transactiontest/application/service/accept/impl/AcceptServiceImpl.java
+++ src/main/java/com/volkruss/transactiontest/application/service/accept/impl/AcceptServiceImpl.java
@@ -1,6 +1,7 @@
 package com.volkruss.transactiontest.application.service.accept.impl;
 
 import com.volkruss.transactiontest.application.service.accept.AcceptService;
+import com.volkruss.transactiontest.application.service.stock.StockService;
 import com.volkruss.transactiontest.controller.AcceptController;
 import com.volkruss.transactiontest.domain.model.accept.Accept;
 import com.volkruss.transactiontest.domain.model.stock.Stock;
@@ -28,6 +29,9 @@ public class AcceptServiceImpl implements AcceptService {
     private StockRepository stockRepository;
 
     @Autowired
+    private StockService stockService;
+
+    @Autowired
     private PlatformTransactionManager txManager;
 
     @Override
@@ -40,31 +44,29 @@ public class AcceptServiceImpl implements AcceptService {
         def.setReadOnly(false);
         TransactionStatus status = txManager.getTransaction(def);
         try{
+            // 別のトランザクション処理の呼出しを行う
+            this.stockService.update();
+
             // ドメインモデルの作成
             Accept accept = new Accept(model.itemId,model.customerName,model.count);
             // 受注の登録をする
             // わかりやすくロールバックを確認するためここで永続化します
             this.acceptRepository.register(accept);
-
             // 在庫の取得
             Stock stock = this.stockRepository.findStockByItemId(accept.getItemId());
-
             // 在庫を減らす(本当の業務なら実在庫を減らすことはせず予定在庫を減らすなど)
             stock.subCount(accept.getCount());
             // 在庫の整合性を確認する
             if(stock.isMinus()){
                 // 適当な例外投げてます
-                throw new Exception();
+                throw new RuntimeException();
             }
             // 在庫の更新をする
             this.stockRepository.save(stock);
-
-
         } catch (RuntimeException e){
             txManager.rollback(status);
             throw e;
         }
         txManager.commit(status);
-
     }
 }
